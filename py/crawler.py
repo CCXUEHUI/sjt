306eeb727e06d7f3d7cb427b3bc933a06559a667
CCXUEHUI <87299715+CCXUEHUI@users.noreply.github.com>
2025-11-18 14:17:37 +0800

    Update crawler.py

--- py/crawler.py
+++ py/crawler.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from PIL import Image
 from io import BytesIO
 
-BASE_URL = "https://m.tuiimg.com/meinv/list_4.html"
+BASE_URL = "https://m.tuiimg.com/meinv/list_5.html"
 TXT_PATH = "/images/files.txt"
 
 HEADERS = {
